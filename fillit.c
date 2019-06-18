6f7e53392d034951f00d28f301363d823820d509
Timon Kelsie <tkelsie@student.21-school.ru>
2019-06-18 18:06:36 +0300

    add smthg

--- fillit.c
+++ fillit.c
@@ -6,25 +6,48 @@
 /*   By: tkelsie <tkelsie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 14:07:35 by tkelsie           #+#    #+#             */
-/*   Updated: 2019/06/18 14:44:43 by tkelsie          ###   ########.fr       */
+/*   Updated: 2019/06/18 18:06:16 by tkelsie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char	*reading(int ac, char *av)
+static int	counter(char *s, int ch)
+{
+	int		nb;
+	char	*str;
+
+	nb = 0;
+	str = (char *)s;
+	while (*str)
+	{
+		if (*str != '.' & *str != '\n' & *str != '#')
+			return (-1);
+		if (*str == ch)
+			nb++;
+		str++;
+	}
+	return (nb);
+}
+
+int	main(int ac, char **av)
 {
 	int		fd;
 	char	buffer[BUFF_SIZE];
+	int		k;
+	char	*tmp;
 
 	fd = open(av[1], O_RDONLY);
-	if (fd == -1 || read(fd, buffer, BUFF_SIZE) < 0 || ac > 2)
+	k = read(fd, &buffer, BUFF_SIZE);
+	tmp = ft_strnew(k+1);
+	tmp = ft_strncpy(tmp, buffer, k);
+	tmp[k] = '\0';
+	if (fd == -1 || k < 21 || k > 546 || k % 21 != 0 ||
+	counter(tmp, '\n') % 5 != 0 || counter(tmp, '#') % 4 != 0 ||
+	counter(tmp, '.') % 12 != 0)
+	{
+		write(1, "error\n", 6);
 		return (-1);
-	buffer[BUFF_SIZE] = '\0';
-	return (NULL); //
-}
-
-char	*fillit(char *str)
-{
-	return (NULL);
+	}
+	return (0);
 }
--- fillit.h
+++ fillit.h
@@ -6,11 +6,20 @@
 /*   By: tkelsie <tkelsie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 14:28:47 by tkelsie           #+#    #+#             */
-/*   Updated: 2019/06/18 14:30:10 by tkelsie          ###   ########.fr       */
+/*   Updated: 2019/06/18 18:02:07 by tkelsie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 # define BUFF_SIZE 547
+
+# include "libft/libft.h"
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 #endif
