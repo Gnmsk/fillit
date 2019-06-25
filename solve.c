2b2568e977581592c2b1c937c8e7b0ea88b7a299
Anna <zhuravlevahanna@gmail.com>
2019-06-25 15:27:24 +0300

    a

--- solve.c
+++ solve.c
@@ -15,51 +15,55 @@
 //	return(NULL);
 //}
 
-int	*searchempty(t_map *mapa, int size)
+int	searchempty(t_map *mapa, int size)
 {
 	int c;
-	int *yx;
+	int yx;
 
-	yx = (int *)malloc(sizeof(int)*2);
-	yx[0] = -1;
-	yx[1] = -1;
+	yx = -1;
+//	yx = (int *)malloc(sizeof(int)*2);
+//	yx[0] = -1;
+//	yx[1] = -1;
 	c = 0;
 	printf("mapsize %d \n", size);
-	while (c < size*size)
+	while (c < size*size && yx == -1)
 	{
 		if (mapa->current[c/size][c%size] == '.')
-		{	yx[0] = c/size;
-			yx[1] = c%size;
-			printf("empty cell: %d %d\n\n", yx[0],yx[1]);
+		{	yx = c;
+			printf("empty cell: %d %d\n\n", yx/size, yx%size);
 			return(yx);
 		}
 		c++;
 	}
-	printf("\n%d %d\n", yx[0],yx[1]);	
+	printf("\n%d %d\n", yx/size, yx%size);
 	return (yx);
 }
 
 char	**solving(t_fgrs **memory, t_map *mapa, int figuresize, int mapsize, int figurenumber)
 {
-	int	*empty;
+	int	empty;
 	t_fgrs 	**temp;
+	int x;
+	int y;
 	int	i;
 
 	i = 0;
 	temp = memory;
 	empty = searchempty(mapa, mapsize);
+	y = empty/mapsize;
+	x = empty%mapsize;
 	//mapa->previous1 = mapa->previous;
         mapa->previous = mapa->current;
-	if (empty[0] + ft_maxsingle(temp[figurenumber]->y, 4) > mapsize || empty[1] + ft_maxsingle(temp[figurenumber]->x, 4) > mapsize)
+	if (y + ft_maxsingle(temp[figurenumber]->y, 4) > mapsize || x + ft_maxsingle(temp[figurenumber]->x, 4) > mapsize)
 	      printf("error: map needs to be extended\n");
 	// draw new map
 	else
 	{
 		while(i < 4)
 		{	
-			if ( mapa->current[empty[0] + temp[figurenumber]->y[i]][empty[1] + temp[figurenumber]->x[i]]  == '.')
+			if ( mapa->current[y + temp[figurenumber]->y[i]][x + temp[figurenumber]->x[i]]  == '.')
 			{
-				mapa->current[empty[0] + temp[figurenumber]->y[i]][empty[1] + temp[figurenumber]->x[i]] = temp[figurenumber]->letter;
+				mapa->current[y + temp[figurenumber]->y[i]][x + temp[figurenumber]->x[i]] = temp[figurenumber]->letter;
 				i++;
 			}
 			else
