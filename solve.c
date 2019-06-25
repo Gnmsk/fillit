df0067c5fc090652336098789f8b45237ec1e20e
Anna <zhuravlevahanna@gmail.com>
2019-06-25 14:47:12 +0300

    a

--- solve.c
+++ solve.c
@@ -54,7 +54,7 @@ char	**solving(t_fgrs **memory, t_map *mapa, int figuresize, int mapsize, int fi
 	i = 0;
 	temp = memory;
 	empty = searchempty(mapa, mapsize);
-	mapa->previous1 = mapa->previous;
+	//mapa->previous1 = mapa->previous;
         mapa->previous = mapa->current;
 	if (empty[0] + ft_maxsingle(temp[figurenumber]->y, 4) > mapsize || empty[1] + ft_maxsingle(temp[figurenumber]->x, 4) > mapsize)
 	      printf("error: map needs to be extended\n");
@@ -75,7 +75,8 @@ char	**solving(t_fgrs **memory, t_map *mapa, int figuresize, int mapsize, int fi
 				return (NULL);
 			}
 		}		
-	}	
+	}
+	mapa->previous1 = mapa->previous;	
 	return (NULL);
 }
 
